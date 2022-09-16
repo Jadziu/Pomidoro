caceaaf8534a93cfcec192eb17e437e29a655e22
Pawel <paweljagowdzik@gmail.com>
2022-09-17 00:43:51 +0200

    Some timer 2; buttons GUI update

--- Pomidoro.py
+++ Pomidoro.py
@@ -5,7 +5,6 @@ bg_color = "#db3939"
 fg_color = '#052a61'
 
 
-
 # Initiation GUI
 root = Tk()
 root.title('Pomidoro')
@@ -15,9 +14,14 @@ root.resizable(width=False, height=False)
 body1 = Frame(root, width=600, height=50, bg=bg_color)
 body1.grid(row=0, column=0, columnspan=2, sticky='nesw')
 body1.grid_propagate(False)
-body2 = Frame(root, width=600, height=300, bg=bg_color)
+
+body2 = Frame(root, width=600, height=200, bg=bg_color)
 body2.grid(row=1, column=0, columnspan=2, sticky='nesw')
 body2.grid_propagate(False)
+
+body3 = Frame(root, width=600, height=100, bg=bg_color)
+body3.grid(row=2, column=0, columnspan=2, sticky='nesw')
+body3.grid_propagate(False)
 
 
 def time_now():
@@ -34,6 +38,15 @@ def colon():
     timer_colon = Label(body2, text=':', font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color)
 
 
+def clear():
+    hr1.set('00')
+    mins1.set('00')
+    secs1.set('00')
+
+
+
+
+
 # ROW:0 time
 time_txt = Label(body1, text='TIME: ', font=("DS-digital", 40, 'bold'), bg=bg_color, fg=fg_color)
 time_txt.grid(row=0, column=0)
@@ -43,67 +56,80 @@ time_now()
 
 # ROW:1 timer one
 # veriables:
-hr = StringVar()
-hr.set('00')
-mins = StringVar()
-mins.set('00')
-secs = StringVar()
-secs.set('00')
+hr1 = StringVar()
+hr1.set('00')
+mins1 = StringVar()
+mins1.set('00')
+secs1 = StringVar()
+secs1.set('00')
 
 # TIMER title
 timer1_txt = Label(body2, text='TIMER1: ', font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color)
 timer1_txt.grid(row=1, column=0, sticky='nw')
 
 # T1 hours
-timer1_hr = Entry(body2, textvariable=hr, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
+timer1_hr = Entry(body2, textvariable=hr1, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
                   width=2, relief='flat')
 timer1_hr.grid(row=1, column=1, sticky='nw')
 colon()
 timer_colon.grid(row=1, column=2, sticky='nw')
 
+
 # T1 minutes
-timer1_min = Entry(body2, textvariable=mins, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
+timer1_min = Entry(body2, textvariable=mins1, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
                    width=2, relief='flat')
 timer1_min.grid(row=1, column=3, sticky='nw')
 colon()
 timer_colon.grid(row=1, column=4, sticky='nw')
 
 # T1 seconds
-timer1_sec = Entry(body2, textvariable=secs, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
+timer1_sec = Entry(body2, textvariable=secs1, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
                    width=2, relief='flat')
 timer1_sec.grid(row=1, column=5, sticky='nw')
 
 # ROW:2 timer two
 # veriables:
-hr = StringVar()
-hr.set('00')
-mins = StringVar()
-mins.set('00')
-secs = StringVar()
-secs.set('00')
+hr2 = StringVar()
+hr2.set('00')
+mins2 = StringVar()
+mins2.set('00')
+secs2 = StringVar()
+secs2.set('00')
 
 # TIMER title
-timer1_txt = Label(body2, text='TIMER1: ', font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color)
-timer1_txt.grid(row=2, column=0, sticky='nw')
+timer2_txt = Label(body2, text='TIMER2: ', font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color)
+timer2_txt.grid(row=2, column=0, sticky='nw')
 
 # T1 hours
-timer1_hr = Entry(body2, textvariable=hr, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
+timer2_hr = Entry(body2, textvariable=hr2, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
                   width=2, relief='flat')
-timer1_hr.grid(row=2, column=1, sticky='nw')
+timer2_hr.grid(row=2, column=1, sticky='nw')
 colon()
 timer_colon.grid(row=2, column=2, sticky='nw')
 
 # T1 minutes
-timer1_min = Entry(body2, textvariable=mins, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
+timer2_min = Entry(body2, textvariable=mins2, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
                    width=2, relief='flat')
-timer1_min.grid(row=2, column=3, sticky='nw')
+timer2_min.grid(row=2, column=3, sticky='nw')
 colon()
 timer_colon.grid(row=2, column=4, sticky='nw')
 
 # T1 seconds
-timer1_sec = Entry(body2, textvariable=secs, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
+timer2_sec = Entry(body2, textvariable=secs2, font=("DS-digital", 60, 'bold'), bg=bg_color, fg=fg_color,
                    width=2, relief='flat')
-timer1_sec.grid(row=2, column=5, sticky='nw')
+timer2_sec.grid(row=2, column=5, sticky='nw')
+
+# MODE SELECTION:
+
+# START BUTTON:
+start_btn = Button(body3, text='START', font=("DS-digital", 30, 'bold'))
+start_btn.grid(row=3, column=0, padx=20)
+
+stop_btn = Button(body3, text='STOP', font=("DS-digital", 30, 'bold'))
+stop_btn.grid(row=3, column=1, padx=20)
+
+clear_btn = Button(body3, text='CLEAR', font=("DS-digital", 30, 'bold'), command=clear)
+clear_btn.grid(row=3, column=3, padx=20)
 
 
 root.mainloop()
